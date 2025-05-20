70e4b60f48834b80ae448299b7ec0988edf3ab7a
Lukari <lukari87@gmail.com>
2025-05-20 23:14:42 +0700

    main

--- app/src/main/java/tiil/edu/cuoiki/MainActivity.java
+++ app/src/main/java/tiil/edu/cuoiki/MainActivity.java
@@ -1,6 +1,8 @@
 package tiil.edu.cuoiki;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Áp insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Khởi tạo Spinner
+        Spinner spinner = findViewById(R.id.spinnerPromptTemplate);
+        String[] promptTemplates = {
+                "Dịch tự nhiên",
+                "Dịch sát nghĩa",
+                "Dịch sáng tạo",
+                "Tóm tắt ý chính"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                promptTemplates
+        );
+
+        spinner.setAdapter(adapter);
     }
 }
